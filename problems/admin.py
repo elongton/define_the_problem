99d74667a5f705f11392ac5f4cd489ac965a0b48
Library - Periodicals <bstdept@richva.ci.richmond.va.us>
2017-06-12 15:33:39 -0400

    fixed line in admin.py

--- problems/admin.py
+++ problems/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Problem, Comment
 # Register your models here.
 
-admin.site.register(Problem, Comment)
+admin.site.register(Problem)
+admin.site.register(Comment)
