7e17c81ab9adfe4c90f7aa47c1cedc0fe03b299b
Max Longton <elongton@vt.edu>
2017-06-16 15:45:18 -0400

    updated create view

--- problems/views.py
+++ problems/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.views.generic import (View, TemplateView,
                                   CreateView, DetailView)
 from . import models
@@ -10,6 +13,11 @@ class ProblemCreateView(CreateView):
     model = models.Problem
     fields = ('text',)
     template_name = 'problems/problem_add.html'
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super(ProblemCreateView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse("users:login"))
 
 class ProblemDetailView(DetailView):
     model = models.Problem
