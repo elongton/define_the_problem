476e3f64867867eb9278f97bd74dcfaf73c83da7
Max Longton <elongton@vt.edu>
2017-07-10 17:26:37 -0400

    changed debug to true

--- definetheproblem/settings/production.py
+++ definetheproblem/settings/production.py
@@ -29,7 +29,7 @@ PROBLEM_TEMPLATES_DIR = os.path.join(BASE_DIR, 'problems/templates')
 SECRET_KEY = os.environ.get('SECRET_KEY','4r(51#_^x+qxole9uwr!&^k%gv()_6-_p7omb(^k%twprd=vi&')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://definetheproblem.herokuapp.com/', 'definetheproblem.com']
 
